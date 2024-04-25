319efcc2e8c0867d1b383efd9e8d0a5acb586d6a
JNUSYJ <lukai7501@naver.com>
2024-04-25 13:53:16 +0900

    240425_04
    저장 기초

---
+++ LoadScene.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SpartaDungeonGame
+{
+    internal class LoadScene : Scene
+    {
+        public override void RunScene()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("불러오기\n");
+            }
+        }
+    }
+}
---
+++ SaveScene.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SpartaDungeonGame
+{
+    internal class SaveScene : Scene
+    {
+        public override void RunScene()
+        {
+            Console.Clear();
+            Console.WriteLine("저장\n");
+            Console.WriteLine(Program.character.ToString);
+
+            Program.sceneManager.GetUserInput(0);
+
+        }
+    }
+}
